04b3358088780c0b24c37335b6730bc888bb48ab
Dennys Lopez <dennyslop@gmail.com>
2026-06-24 12:22:31 -0400

    Se inicia el desarrollo de mostrar documentos por vencer

---
+++ SociePolar.Domain/Entities/DocumentosVencidos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SociePolar.Domain.Entities
+{
+    public class DocumentosVencidos
+    {
+        public int Id { get; set; }
+        public string Modulo { get; set; }
+        public string Documento { get; set; }
+        public string NroDocumento { get; set; }
+        public string Nombre { get; set; }
+        public DateTime? FechaVencimiento { get; set; }
+    }
+}

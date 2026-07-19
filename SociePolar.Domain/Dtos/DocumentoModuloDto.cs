c85555e7c946455143ccf630643d9153e000f56b
Dennys Lopez <dennyslop@gmail.com>
2026-07-19 17:15:31 -0400

    Se continua con el manejo de documentos de soporte

--- SociePolar.Domain/Dtos/DocumentoModuloDto.cs
+++ SociePolar.Domain/Dtos/DocumentoModuloDto.cs
@@ -1,9 +1,15 @@
-﻿namespace SociePolar.Domain.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SociePolar.Domain.Dtos
 {
     public class DocumentoModuloDto
     {
         public int Id { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar una Tipo Documento válida.")]
         public int? TipoDocumentoSoporteId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar una Referencia válida.")]
         public int ReferenciaId { get; set; }
         public string? RutaGoogle { get; set; }
         public string? Comentarios { get; set; }

8b47184bc0423372503baea0e012fc8685d84c60
Dennys Lopez <dennyslop@gmail.com>
2026-08-03 14:54:26 -0400

    Se realiza el manejo de descarga de datos de sociedades activas

---
+++ SociePolar.Domain/Models/MatrizCSV.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SociePolar.Domain.Models
+{
+    public class MatrizCSV()
+    {
+        [Range(0, 1000, ErrorMessage = "El tipo separador es obligatorio.")]
+        public int TipoSeparador { get; set; }
+    }
+}
